d4d207da8d1a3e8e0469eda9f905dc974d079c36
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2024-05-31 09:29:36 +0300

    Making Models & Migrations

---
+++ backend/database/migrations/2024_05_30_110203_create_loans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('loans', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->integer('book_id')->unsigned();
+            $table->foreign('book_id')->references('id')->on('books');
+
+            $table->datetime('loan_date')->nullable();
+            $table->datetime('return_date')->nullable();
+            $table->boolean('loan_status')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('loans');
+    }
+};
